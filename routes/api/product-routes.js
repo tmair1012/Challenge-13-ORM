5737235f3d9553a045cd5094bc7e9d4e2d595111
TylerMair <tyleramair@gmail.com>
2021-11-22 18:55:50 -0700

    added tag and category routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  console.log('======================');
+    Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
